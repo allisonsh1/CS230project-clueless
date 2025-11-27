16fefbe31a59fb609cac25e159fc18a1d654ecfa
Allison <azshi@stu.naperville203.org>
2025-11-27 14:05:58 -0500

    More for DecisionTreePicker

--- DecisionTreePicker.java
+++ DecisionTreePicker.java
@@ -1,12 +1,20 @@
 public class DecisionTreePicker {
-    public static String chooseSeason(boolean isCold) {
-        return null;
-    }
-
+    /**
+     * Looks through all tops in the closet and returns one that matches the requested season. If there are none, throws an exception.
+     * @param closet
+     * @param season
+     * @return
+     */
     public static Top pickTopBySeason(Closet closet, String season) {
         return null;
     }
 
+    /**
+     * Looks through all bottoms in the closet and returns one that matches the requested season. If there are none, throws an exception.
+     * @param closet
+     * @param season
+     * @return
+     */
     public static Bottom pickBottomBySeason(Closet closet, String season) {
         return null;
     }
