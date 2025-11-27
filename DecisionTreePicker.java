d2526f606adb792bd216b972be1be54d3e09f5ec
Allison <azshi@stu.naperville203.org>
2025-11-27 13:58:17 -0500

    Added DecisionTreePicker skeleton code

---
+++ DecisionTreePicker.java
@@ -0,0 +1,13 @@
+public class DecisionTreePicker {
+    public static String chooseSeason(boolean isCold) {
+        return null;
+    }
+
+    public static Top pickTopBySeason(Closet closet, String season) {
+        return null;
+    }
+
+    public static Bottom pickBottomBySeason(Closet closet, String season) {
+        return null;
+    }
+}
