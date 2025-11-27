1970a85c9c3dfb8440039b4e67afe712feee583a
Allison <azshi@stu.naperville203.org>
2025-11-27 14:11:25 -0500

    Changed my mind about decisionTree

--- DecisionTreePicker.java
+++ DecisionTreePicker.java
@@ -1,21 +1,5 @@
 public class DecisionTreePicker {
-    /**
-     * Looks through all tops in the closet and returns one that matches the requested season. If there are none, throws an exception.
-     * @param closet
-     * @param season
-     * @return
-     */
-    public static Top pickTopBySeason(Closet closet, String season) {
-        return null;
-    }
 
-    /**
-     * Looks through all bottoms in the closet and returns one that matches the requested season. If there are none, throws an exception.
-     * @param closet
-     * @param season
-     * @return
-     */
-    public static Bottom pickBottomBySeason(Closet closet, String season) {
         return null;
     }
 }
